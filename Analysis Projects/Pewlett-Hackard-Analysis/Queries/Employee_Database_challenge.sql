8315d836a3e1fa75ae0f6f9f8c734033e639e81e
dwright68 <destinwright68@outlook.com>
2021-10-20 18:06:15 -0500

    adding deliverable 2

--- Analysis Projects/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
+++ Analysis Projects/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -43,3 +43,22 @@ SELECT COUNT(ut.title)
 FROM unique_titles as ut
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+
+-- Mentor eligibilty 
+SELECT DISTINCT ON (e.emp_no)e.emp_no, 
+       e.first_name, 
+       e.last_name, 
+       e.birth_date,
+       de.from_date,
+       de.to_date,
+       t.title
+FROM employees AS e
+-- INTO mentors
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '1/1/9999') AND 
+(e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no
